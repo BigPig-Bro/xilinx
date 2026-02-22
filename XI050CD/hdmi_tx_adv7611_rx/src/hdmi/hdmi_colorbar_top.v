27d8c77e1c6a99263e99963ef78a6b77b5bd4d89
BigPig <1186725127@qq.com>
2026-02-22 14:20:53 +0800

    260222 增加板载HDMI+ADV7611回环例程

---
+++ XI050CD/hdmi_tx_adv7611_rx/src/hdmi/hdmi_colorbar_top.v
@@ -0,0 +1,110 @@
+//****************************************Copyright (c)***********************************//
+//ԭ�Ӹ����߽�ѧƽ̨��www.yuanzige.com
+//����֧�֣�www.openedv.com
+//�Ա����̣�http://openedv.taobao.com 
+//��ע΢�Ź���ƽ̨΢�źţ�"����ԭ��"����ѻ�ȡZYNQ & FPGA & STM32 & LINUX���ϡ�
+//��Ȩ���У�����ؾ���
+//Copyright(C) ����ԭ�� 2018-2028
+//All rights reserved
+//----------------------------------------------------------------------------------------
+// File name:           hdmi_colorbar_top
+// Last modified Date:  2019/7/1 9:30:00
+// Last Version:        V1.1
+// Descriptions:        HDMI������ʾʵ�鶥��ģ��
+//----------------------------------------------------------------------------------------
+// Created by:          ����ԭ��
+// Created date:        2019/7/1 9:30:00
+// Version:             V1.0
+// Descriptions:        The original version
+//
+//----------------------------------------------------------------------------------------
+//****************************************************************************************//
+
+module  hdmi_colorbar_top(
+    input        sys_clk,
+    input        sys_rst_n,
+    
+    output      o_video_clk,    // ��Ƶʱ��
+
+    output       tmds_clk_p,    // TMDS ʱ��ͨ��
+    output       tmds_clk_n,
+    output       tmds_oen,
+    output [2:0] tmds_data_p,   // TMDS ����ͨ��
+    output [2:0] tmds_data_n
+);
+
+//wire define
+wire          pixel_clk;
+wire          pixel_clk_5x;
+wire          clk_locked;
+
+wire  [10:0]  pixel_xpos_w;
+wire  [10:0]  pixel_ypos_w;
+wire  [23:0]  pixel_data_w;
+
+wire          video_hs;
+wire          video_vs;
+wire          video_de;
+wire  [23:0]  video_rgb;
+
+//*****************************************************
+//**                    main code
+//*****************************************************
+
+//����MMCM/PLL IP��
+clk_wiz_0  clk_wiz_0(
+    .clk_in1        (sys_clk),
+    .clk_out1       (pixel_clk),        //����ʱ��
+    .clk_out2       (pixel_clk_5x),     //5������ʱ��
+    
+    .reset          (~sys_rst_n), 
+    .locked         (clk_locked)
+);
+
+assign o_video_clk = pixel_clk;
+
+//������Ƶ��ʾ����ģ��
+video_driver  u_video_driver(
+    .pixel_clk      ( pixel_clk ),
+    .sys_rst_n      ( sys_rst_n ),
+
+    .video_hs       ( video_hs ),
+    .video_vs       ( video_vs ),
+    .video_de       ( video_de ),
+    .video_rgb      ( video_rgb ),
+	.data_req		(),
+
+    .pixel_xpos     ( pixel_xpos_w ),
+    .pixel_ypos     ( pixel_ypos_w ),
+	.pixel_data     ( pixel_data_w )
+);
+
+//������Ƶ��ʾģ��
+video_display  u_video_display(
+    .pixel_clk      (pixel_clk),
+    .sys_rst_n      (sys_rst_n),
+
+    .pixel_xpos     (pixel_xpos_w),
+    .pixel_ypos     (pixel_ypos_w),
+    .pixel_data     (pixel_data_w)
+    );
+
+//����HDMI����ģ��
+dvi_transmitter_top u_rgb2dvi_0(
+    .pclk           (pixel_clk),
+    .pclk_x5        (pixel_clk_5x),
+    .reset_n        (sys_rst_n & clk_locked),
+                
+    .video_din      (video_rgb),
+    .video_hsync    (video_hs), 
+    .video_vsync    (video_vs),
+    .video_de       (video_de),
+                
+    .tmds_clk_p     (tmds_clk_p),
+    .tmds_clk_n     (tmds_clk_n),
+    .tmds_data_p    (tmds_data_p),
+    .tmds_data_n    (tmds_data_n), 
+    .tmds_oen       (tmds_oen)                        
+    );
+
+endmodule 
